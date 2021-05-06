fb75054a443cafc2f450d0ce0f3d043cce670d6d
Tom <tomm92@outlook.it>
2021-05-06 12:50:25 +0200

    Aggiungere i file di progetto.

---
+++ Esame/Data/BancaDati.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Esame.Data
+{
+	public class BancaDati : DbContext
+	{
+		public DbSet<Piatto> piatti { get; set; }
+
+		public BancaDati(DbContextOptions options) : base(options) { }
+	}
+}
---
+++ Esame/Data/Piatto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Esame.Data
+{
+    public class Piatto
+    {
+        public int id { get; set; }
+        public string tipo_portata { get; set; }
+        public string allergeni { get; set; }
+        public string stagione { get; set; }
+        public string prezzo { get; set; }
+    }
+}
